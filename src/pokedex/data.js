0844fac948ae229f0a8469b332163967ce8384af
eric-jacobson <ecjacobson9@gmail.com>
2024-04-18 20:14:57 -0600

    can cycle through the first few sprites of the pokedex from local data

---
+++ src/pokedex/data.js
@@ -0,0 +1,38 @@
+export const urls = [
+  {
+    name: "Bulbasaur",
+    url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png",
+  },
+  {
+    name: "Ivysaur",
+    url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/2.png",
+  },
+  {
+    name: "Venusaur",
+    url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/3.png",
+  },
+  {
+    name: "Charmander",
+    url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/4.png",
+  },
+  {
+    name: "Charmeleon",
+    url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/5.png",
+  },
+  {
+    name: "Charizard",
+    url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/6.png",
+  },
+  {
+    name: "Squirtle",
+    url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/7.png",
+  },
+  {
+    name: "Wartortle",
+    url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/8.png",
+  },
+  {
+    name: "Blastoise",
+    url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/9.png",
+  },
+];
